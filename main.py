a14a0431c631c0295264ea383d1f7ae0fa3d46bb
root <root@adjambo>
2024-10-21 17:46:21 +0200

    Le test réussit, car la méthode affiche() fonctionne pour la séquence 1 à 100

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 def affiche():
-    return ""
+    resultat = ""
+    for i in range(1, 101):
+        if i % 15 == 0:
+            resultat += "FrisBee"
+        elif i % 3 == 0:
+            resultat += "Fizz"
+        elif i % 5 == 0:
+            resultat += "Buzz"
+        else:
+            resultat += str(i)
+    return resultat
