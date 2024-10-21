024640a75fd9eff453d206b56831dbb6f8ba5176
root <root@adjambo>
2024-10-21 18:27:36 +0200

    Le test réussit, car la méthode affiche(n1, n2) fonctionne correctement pour deux paramètres

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def affiche(n):
+def affiche(n1,n2):
     result = []
-    for i in range(1, n + 1):
+    for i in range(n1, n2 + 1):
         if i % 15 == 0:
             result.append("FrisBee")
         elif i % 3 == 0:
