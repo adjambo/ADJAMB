a4b0b0293670281643361b3f151f924c7cb2f735
root <root@adjambo>
2024-10-21 18:07:14 +0200

    Le test réussit, car la méthode affiche(n) fonctionne pour le paramètre n=15

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def affiche():
+def affiche(n):
     result = []
-    for i in range(1, 101):
+    for i in range(1, n + 1):
         if i % 15 == 0:
             result.append("FrisBee")
         elif i % 3 == 0:
@@ -10,4 +10,5 @@ def affiche():
         else:
             result.append(str(i))
     return ''.join(result)
+
 
