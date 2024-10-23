7b8cc4c31310437150d13787b5c71dce4980dcdc
root <root@adjambo>
2024-10-23 22:12:49 +0200

    decrypt() échoue car retourne une chaîne vide

--- Exercice2/main_crypt.py
+++ Exercice2/main_crypt.py
@@ -3,3 +3,6 @@ def crypt(message, pas=1):
     for char in message:
         result += chr(ord(char) + pas ) 
     return result
+
+def decrypt(message):
+    return ""  # Fonction vide pour forcer l'échec du test
--- Exercice2/test_crypt.py
+++ Exercice2/test_crypt.py
@@ -1,12 +1,16 @@
 import unittest
-from main_crypt import crypt
+from main_crypt import crypt, decrypt
 
 class TestCrypt(unittest.TestCase):
+    def test_crypt(self):
+        self.assertEqual(crypt("abc1"), "bcd2")
+    
     def test_crypt_avec_pas(self):
-        # Teste le cryptage avec un paramètre de décalage "pas"
         self.assertEqual(crypt("abc1", 2), "cde3")
+
+    def test_decrypt(self):
+        # Teste le décryptage pour obtenir le message original
+        self.assertEqual(decrypt("bcd2"), "abc1")
 
 if __name__ == '__main__':
     unittest.main()
-
-
