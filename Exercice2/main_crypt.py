19bf6d1c61783c0283e1cdf4e6651378fd860470
root <root@adjambo>
2024-10-23 21:50:50 +0200

    crypt() n'accepte qu'un argument, 2 attendus

--- Exercice2/main_crypt.py
+++ Exercice2/main_crypt.py
@@ -1,5 +1,5 @@
 def crypt(message):
     result = ""
     for char in message:
-        result += chr(ord(char) + 1)
+        result += chr(ord(char) + 1)  # Ne prend pas encore en compte "pas"
     return result
--- Exercice2/test_crypt.py
+++ Exercice2/test_crypt.py
@@ -2,10 +2,11 @@ import unittest
 from main_crypt import crypt
 
 class TestCrypt(unittest.TestCase):
-    def test_crypt(self):
-        # Teste le cryptage sans paramètre "pas"
-        self.assertEqual(crypt("abc1"), "bcd2")
+    def test_crypt_avec_pas(self):
+        # Teste le cryptage avec un paramètre de décalage "pas"
+        self.assertEqual(crypt("abc1", 2), "cde3")
 
 if __name__ == '__main__':
     unittest.main()
+
 
